88b20876244e500d2765e9e30323c8dad6652a2f
coderdr31 <1683751393@qq.com>
2017-02-08 16:33:09 +0800

    change shellscript to jShellscript

--- myshellscript.sh
+++ myshellscript.sh
@@ -5,26 +5,29 @@
 ## cd
 ##################################################################
 alias cdws="cd ~/Desktop/dworkspace"
-alias cddctf="cd ~/github/DGithub/DCtf"
+alias cshare="cd /media/dr/文档/Share"
+
 alias cddg="cd ~/github/DGithub"
+alias cddctf="cd ~/github/DGithub/DCtf"
 alias cddt="cd ~/github/DGithub/DTemplate"
 alias cddcpp="cd ~/github/DGithub/DTemplate/cpp"
+alias cddpc="cd ~/github/DGithub/DPc/config_pc_dr"
 alias cddshell="cd ~/github/drshellscript"
-alias cdjshell="cd ~/github/shellscript"
-alias cdog="cd ~/github/otherpeople"
+
 alias cdjg="cd ~/github/JGithub"
-alias cdjsc="cd ~/github/JGithub/jvim/UltiSnips"
-alias cshare="cd /media/dr/文档/Share"
+alias cdjshell="cd ~/github/jShellscript"
 alias cdjpc='cd ~/github/JGithub/jPC'
 alias cdjserver='cd ~/github/JGithub/jServer'
 alias cdjtemplate="cd ~/github/JGithub/jTemplate"
+alias cdjsc="cd ~/github/JGithub/jvim/UltiSnips"
 alias cdjvim="cd ~/github/JGithub/jvim/vimrc"
+alias cdog="cd ~/github/otherpeople"
 
 ##################################################################
 ## vim
 ##################################################################
-alias vimjms="vim ~/github/shellscript/myscript"
-alias vimms="vim ~/.config/jshellscript/myshellscript.sh"
+alias vimjms="vim ~/github/jShellscript/myscript"
+alias vimms="cd ~/github/drshellscript/ && vim myshellscript.sh"
 
 ##################################################################
 ## ssh
@@ -52,7 +55,13 @@ alias showipt="sudo iptables -n -v -L -t filter |grep -i 'spt:1080' |awk -F' ' '
 ##
 ##################################################################
 alias flt="cd ~/floodlight/ && java -jar target/floodlight.jar"
-alias qrsync="~/github/otherpeople/qrsync/qrsync"
+alias qrsync="~/github/otherpeople/qrsync/qrsync ~/github/DGithub/DPc/config_pc_dr/src/qrsync_config.json"
+alias cl="clear"
+# alias dvim="docker run -i -t -v /home/dr/Desktop/dvim/vimrc:/home/coderdr31/.vimrc -v /home/dr/Desktop/dvim/vim:/home/coderdr31/.vim -u coderdr31 -w /home/coderdr31 coderdr31/ubuntu:16.04" #有bug
+alias dvim='docker run -i -t -v /home/dr/Desktop/dvim/vim:/home/coderdr31/.vim -u coderdr31 -w /home/coderdr31 coderdr31/ubuntu:16.04 /bin/bash -c \
+            "ln -s /home/coderdr31/.vim/dvimrc /home/coderdr31/.vimrc \
+            && echo \"source /home/coderdr31/.vim/myshellscript.sh\" >> /home/coderdr31/.bashrc \
+            && /bin/bash"'
 
 ##################################################################
 ## cd

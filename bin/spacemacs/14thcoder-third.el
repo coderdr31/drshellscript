59674376dfe4bac39adddad5d31139a1ce3a9cf8
Ruipeng Jia <352111644@qq.com>
2017-12-23 11:48:59 +0800

    fix emacs f8

--- bin/spacemacs/14thcoder-third.el
+++ bin/spacemacs/14thcoder-third.el
@@ -1,3 +1,4 @@
 
 (evil-ex-define-cmd "panma[th]" (lambda () (interactive) (shell-command (format "pandoc --latex-engine=xelatex --highlight-style=pygments --template=dr_pandoc_math-manual.tex -o ./pdf/%s.pdf %s" (file-name-sans-extension (file-name-nondirectory (buffer-file-name))) (file-name-nondirectory (buffer-file-name))))))
+(setenv "PAGER" "/bin/cat")
 (provide '14thcoder-third)
